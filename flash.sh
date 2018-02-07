118960e40d6584fbb137aa55387eb70390d3eed6
koios <koios@programmingpanda.net>
2018-02-07 19:03:32 +0100

    Display build phases in one-step flashing tool

--- flash.sh
+++ flash.sh
@@ -1,12 +1,24 @@
 #!/bin/bash -e
 
+function heading() {
+  echo
+  echo $1
+  echo "============================"
+}
+
+heading "= Building firmware"
 cd firmware_docker
 ./docker_rm.sh
+heading "== Creating container which builds the firmware"
 ./docker_build.sh
+heading "== Building firmware"
 ./docker_run.sh
 
 cd ..
 
+heading "= Flashing firmware"
 cd loader_docker
+heading "== Creating container with flashing tool"
 ./docker_build.sh
+heading "== Flashing firmware"
 ./docker_run.sh
